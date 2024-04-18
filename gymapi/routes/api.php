54cf6082d8be529ec672ee6e6ad2a61ccd5c6783
vico171 <a22311031@uthermosillo.edu.mx>
2024-04-18 13:27:59 -0700

    terminada

--- gymapi/routes/api.php
+++ gymapi/routes/api.php
@@ -32,5 +32,5 @@ Route::get('/rutinas/buscar/', [RutinasController::class, 'search']);
 Route::get('/rutinas/{id}', [RutinasController::class, 'item']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/user/{id}', [UserController::class, 'item']);
-Route::put('/update/{id}', [RutinasController::class, 'updateRutina']);
+Route::post('/update/{id}', [RutinasController::class, 'updateRutina']);
 Route::post('/perfil/{id}', [UserController::class, 'updatePerfil']);
